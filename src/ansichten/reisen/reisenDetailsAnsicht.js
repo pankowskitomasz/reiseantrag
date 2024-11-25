71f00e08e47c04a60c609bd943c8a7bbbcb1dccc
tomaszpankowski <pankowskitomasz@mailo.com>
2024-11-25 12:11:09 +0100

    anwendungsrouting

---
+++ src/ansichten/reisen/reisenDetailsAnsicht.js
@@ -0,0 +1,12 @@
+import {Container } from "react-bootstrap";
+
+function ReisenDetailsAnsicht(){
+    return(
+        <Container>
+            <h1>Reisen Details Ansicht</h1>
+
+        </Container>
+    );
+}
+
+export default ReisenDetailsAnsicht;
---
+++ src/ansichten/reisen/reisenListeAnsicht.js
@@ -0,0 +1,12 @@
+import {Container } from "react-bootstrap";
+
+function ReisenListeAnsicht(){
+    return(
+        <Container>
+            <h1>Reisen Liste Ansicht</h1>
+
+        </Container>
+    );
+}
+
+export default ReisenListeAnsicht;
