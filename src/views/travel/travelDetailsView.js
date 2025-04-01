8deb89d1bf0aeb2fb65a7e951707f631055263ad
pankowskitomasz <pankowski.email@gmail.com>
2025-04-01 06:21:19 +0200

    routing, layout: signin, signout, passwordupdate, restorepassword

---
+++ src/views/travel/travelDetailsView.js
@@ -0,0 +1,12 @@
+import {Container } from "react-bootstrap";
+
+function TravelDetailsView(){
+    return(
+        <Container>
+            <h1>Travel Details View</h1>
+
+        </Container>
+    );
+}
+
+export default TravelDetailsView;
---
+++ src/views/travel/travelListView.js
@@ -0,0 +1,12 @@
+import {Container } from "react-bootstrap";
+
+function TravelListView(){
+    return(
+        <Container>
+            <h1>Travel List View</h1>
+
+        </Container>
+    );
+}
+
+export default TravelListView;
